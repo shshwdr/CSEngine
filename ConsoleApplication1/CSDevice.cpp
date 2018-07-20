63c4cecb7e2ddeea7abe90721153dbcf217f394a
shshwdr <cs.vivian04@gmail.com>
2018-07-19 20:33:27 -0700

    move draw function to CSDevice

---
+++ ConsoleApplication1/CSDevice.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+
+#include "CSDevice.h"
+
+
+CSDevice::CSDevice() {}
+CSDevice::~CSDevice() {}
+void CSDevice::InitDevice(HDC hdc, int screenWidtch, int screenHeight) {
+	screenHDC = hdc;
+}
+
+void CSDevice::DrawLine(int x0, int y0, int x1, int y1)
+{
+	//Bresenham
+	//slope = dy/dx
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	float errorValue = 0;
+	for (int x = x0, y = y0;x <= x1;x++) {
+		DrawPixel(x, y);
+		errorValue += (float)dy / dx;
+		if (errorValue > 0.5) {
+			errorValue -= 1;
+			y++;
+		}
+	}
+}
+
+inline void CSDevice::DrawPixel(int x, int y)
+{
+	SetPixel(screenHDC, x, y, RGB(255, 0, 0));
+}
---
+++ ConsoleApplication1/CSDevice.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "stdafx.h"
+#include <windows.h>
+class CSDevice
+{
+private:
+	int deviceWidth;
+	int deviceHeight;
+	HDC screenHDC;
+
+public:
+	CSDevice();
+	~CSDevice();
+	void InitDevice(HDC hdc, int screenWidtch, int screenHeight);
+	void DrawLine(int x0, int y0, int x1, int y1);
+	void DrawPixel(int x0, int y0);
+};
